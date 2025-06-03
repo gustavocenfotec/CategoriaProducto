5d9338674fe209599beb16dd032264ef97aa032f
gustavoc <gustavosays@gmail.com>
2025-06-03 00:02:00 -0600

    update categorias
    update categorias

---
+++ src/main/java/com/project/demo/logic/entity/categoria/CategoriaSeeder.java
@@ -0,0 +1,66 @@
+package com.project.demo.logic.entity.categoria;
+
+
+import com.project.demo.logic.entity.rol.Role;
+import com.project.demo.logic.entity.rol.RoleEnum;
+import com.project.demo.logic.entity.rol.RoleRepository;
+import com.project.demo.logic.entity.user.User;
+import com.project.demo.logic.entity.user.UserRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CategoriaSeeder implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final CategoriaRepository categoriaRepository;
+
+
+    public CategoriaSeeder(
+            CategoriaRepository categoriaRepository
+    ) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        this.createCategoria();
+    }
+
+    private void createCategoria() {
+        Categoria categoria1 = new Categoria();
+        categoria1.setNombre("Lacteos");
+        categoria1.setDescripcion("Categoria dedicada a lechee");
+
+        Optional<Categoria> optionalCategoria = categoriaRepository.findByNombre(categoria1.getNombre());
+
+        if (optionalCategoria.isPresent()) {
+            return;
+        }
+
+        var categoria = new Categoria();
+        categoria.setNombre(categoria1.getNombre());
+        categoria.setDescripcion(categoria1.getDescripcion());
+
+        categoriaRepository.save(categoria);
+
+        Categoria categoria2 = new Categoria();
+        categoria2.setNombre("Vegetales");
+        categoria2.setDescripcion("Categoria dedicada a veggies");
+
+        optionalCategoria = categoriaRepository.findByNombre(categoria2.getNombre());
+
+        if (optionalCategoria.isPresent()) {
+            return;
+        }
+
+        categoria = new Categoria();
+        categoria.setNombre(categoria2.getNombre());
+        categoria.setDescripcion(categoria2.getDescripcion());
+
+        categoriaRepository.save(categoria);
+
+    }
+}
---
+++ src/main/java/com/project/demo/logic/entity/producto/ProductoSeeder.java
@@ -0,0 +1,4 @@
+package com.project.demo.logic.entity.producto;
+
+public class ProductoSeeder {
+}
