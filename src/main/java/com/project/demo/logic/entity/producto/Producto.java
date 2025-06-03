af54e851946cddd0268e93d4d0d91c4511efac55
gustavoc <gustavosays@gmail.com>
2025-06-02 22:44:05 -0600

    Modelos funcionales
    Modelos funcionales

---
+++ src/main/java/com/project/demo/logic/entity/producto/Producto.java
@@ -0,0 +1,95 @@
+package com.project.demo.logic.entity.producto;
+
+import com.project.demo.logic.entity.categoria.Categoria;
+import com.project.demo.logic.entity.order.Order;
+import com.project.demo.logic.entity.rol.Role;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+
+@Table(name = "producto")
+@Entity
+
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int cantidad;
+
+    /*@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "categoria_id", referencedColumnName = "id", nullable = false)
+    private Categoria categoria;*/
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "categoria_id", referencedColumnName = "id", nullable = false)
+    private Categoria categoria;
+
+
+
+    public Producto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+   /* public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }*/
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", precio=" + precio +
+                ", cantidad=" + cantidad +
+                //", categoria=" + categoria +
+                '}';
+    }
+}
