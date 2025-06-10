58ce36da511bf1d3fe637b2b853f0959079ba8e4
gustavoc <gustavosays@gmail.com>
2025-06-09 20:44:49 -0600

    Actualizacion de Password
    Actualizacion de Password

--- src/main/java/com/project/demo/rest/producto/ProductosRestController.java
+++ src/main/java/com/project/demo/rest/producto/ProductosRestController.java
@@ -17,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
