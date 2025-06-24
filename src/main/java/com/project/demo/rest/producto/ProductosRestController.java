f52ddecc2110f28f10b91633f2e9a03f892616a7
gustavocenfotec <galvaradoc@ucenfotec.ac.cr>
2025-06-24 03:17:08 -0600

    Update Delete
    Update Delete

--- src/main/java/com/project/demo/rest/producto/ProductosRestController.java
+++ src/main/java/com/project/demo/rest/producto/ProductosRestController.java
@@ -108,6 +108,26 @@ public class ProductosRestController {
 
     
 
+//    @DeleteMapping("/{productoId}")
+//    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+//    public ResponseEntity<?> deleteProducto(@PathVariable Long productoId, HttpServletRequest request) {
+//        Optional<Producto> foundProducto = productoRepository.findById(productoId);
+//        if(foundProducto.isPresent()) {
+//            Producto producto = foundProducto.get();
+//            Categoria categoria = producto.getCategoria();
+//            if (categoria != null) {
+//                categoria.getProductos().remove(producto);
+//                producto.setCategoria(null);
+//                categoriaRepository.save(categoria);
+//            }
+//            return new GlobalResponseHandler().handleResponse("Producto borrado de forma correcta",
+//                    foundProducto.get(), HttpStatus.OK, request);
+//        } else {
+//            return new GlobalResponseHandler().handleResponse("Producto id " + productoId + " not found"  ,
+//                    HttpStatus.NOT_FOUND, request);
+//        }
+//    }
+
     @DeleteMapping("/{productoId}")
     @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
     public ResponseEntity<?> deleteProducto(@PathVariable Long productoId, HttpServletRequest request) {
@@ -119,6 +139,9 @@ public class ProductosRestController {
                 categoria.getProductos().remove(producto);
                 producto.setCategoria(null);
                 categoriaRepository.save(categoria);
+                productoRepository.deleteById(productoId);
+            }else {
+                productoRepository.deleteById(productoId);
             }
             return new GlobalResponseHandler().handleResponse("Producto borrado de forma correcta",
                     foundProducto.get(), HttpStatus.OK, request);
